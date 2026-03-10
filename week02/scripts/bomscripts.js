bac9204c8d6433c675b63b5cf54a257352cb03bd
douglasc2001 <douglasc_2001@hotmail.com>
2026-03-10 17:23:29 -0600

    Learning activity week 2

---
+++ week02/scripts/bomscripts.js
@@ -0,0 +1,25 @@
+const inputElement = document.querySelector("#favchap");
+const buttonElement = document.querySelector("button");
+const listElement = document.querySelector("#list");
+
+buttonElement.addEventListener('click', function()
+{
+if (inputElement.value.trim() !==''){
+    const li = document.createElement('li');
+    li.textContent = inputElement.value;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+    deleteButton.addEventListener ('click', function() {
+    listElement.removeChild(li);
+    inputElement.focus();
+    
+});
+
+li.appendChild(deleteButton);
+listElement.appendChild(li);
+inputElement.value='';
+}
+inputElement.focus();
+});
+
+
